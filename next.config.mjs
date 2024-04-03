d8a563461ebd19d6088c3adcd8198f0d5b794111
Adrián Blesa Moreno <adrian.blesa.moreno@gmail.com>
2024-04-03 15:05:10 +0200

    gh-pages config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/blanco",
+    output: "export",
+    reactStrictMode: true
+};
 
 export default nextConfig;
